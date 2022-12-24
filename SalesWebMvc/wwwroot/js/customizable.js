c8956f1d6b485a52aa393f87d1ad4936c12f9c9b
Allan Fantoni <allan.fantoni@outlook.com>
2022-12-24 01:04:15 -0300

    Added support for theme switcher

---
+++ SalesWebMvc/wwwroot/js/customizable.js
@@ -0,0 +1,14 @@
+﻿document
+    .querySelectorAll("input[type='radio']")
+    .forEach(function (input) {
+        input.onclick = function () {
+            if (input.id == 'light') {
+                $("link[href*='bootstrap-darkly.css']").prop('disabled', true);
+                $("link[href*='bootstrap-flatly.css']").prop('disabled', false);
+            }
+            else if (input.id == 'dark') {
+                $("link[href*='bootstrap-flatly.css']").prop('disabled', true);
+                $("link[href*='bootstrap-darkly.css']").prop('disabled', false);
+            }
+        }
+    });
