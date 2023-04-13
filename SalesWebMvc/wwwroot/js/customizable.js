03cb3c8674edf9c5738ca8c925e2a05c936b2fdc
Allan Fantoni <allan.fantoni@outlook.com>
2023-04-13 00:55:37 -0300

    Added cookie for theme selected

--- SalesWebMvc/wwwroot/js/customizable.js
+++ SalesWebMvc/wwwroot/js/customizable.js
@@ -2,13 +2,33 @@
     .querySelectorAll("input[type='radio']")
     .forEach(function (input) {
         input.onclick = function () {
-            if (input.id == 'light') {
-                $("link[href*='bootstrap-darkly.css']").prop('disabled', true);
-                $("link[href*='bootstrap-flatly.css']").prop('disabled', false);
-            }
-            else if (input.id == 'dark') {
-                $("link[href*='bootstrap-flatly.css']").prop('disabled', true);
-                $("link[href*='bootstrap-darkly.css']").prop('disabled', false);
-            }
+            document.cookie = 'sel_theme=' + input.id + ';';
+            setTheme(input.id);
         }
     });
+
+window.onload = function () {
+
+    var cookie_pos = document.cookie.indexOf('sel_theme=');
+
+    if (cookie_pos != -1) {
+
+        var cookie_flavor = document.cookie.substring(cookie_pos + 10);
+        setTheme(cookie_flavor);
+    }
+}
+
+function setTheme(name) {
+    if (name == 'light') {
+        $("link[href*='bootstrap-darkly.css']").prop('disabled', true);
+        document.getElementById("dark").checked = false;
+        $("link[href*='bootstrap-flatly.css']").prop('disabled', false);
+        document.getElementById("light").checked = true;
+    }
+    else if (name == 'dark') {
+        $("link[href*='bootstrap-flatly.css']").prop('disabled', true);
+        document.getElementById("light").checked = false;
+        $("link[href*='bootstrap-darkly.css']").prop('disabled', false);
+        document.getElementById("dark").checked = true;
+    }
+}
